0ee673b199093d5de47d75e7244edfb9373debd7
Philipp Thomas <philipp.thomas@idealo.de>
2022-12-18 13:16:10 +0100

    switch to webflux security to use Netty instead of Tomcat and get rid of starter-web dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-security")
--- src/main/kotlin/name/hennr/cctrayhub/cctray/security/DefaultNoAuthWebSecurityConfiguration.kt
+++ src/main/kotlin/name/hennr/cctrayhub/cctray/security/DefaultNoAuthWebSecurityConfiguration.kt
@@ -3,16 +3,18 @@ package name.hennr.cctrayhub.cctray.security
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
 
 @Configuration
+@EnableWebFluxSecurity
 @ConditionalOnMissingBean(BasicAuthWebSecurityConfiguration::class)
 class DefaultNoAuthWebSecurityConfiguration {
 
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
-        http.authorizeHttpRequests().anyRequest().permitAll()
+    fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        http.authorizeExchange { it.anyExchange().permitAll() }
         return http.build()
     }
 
