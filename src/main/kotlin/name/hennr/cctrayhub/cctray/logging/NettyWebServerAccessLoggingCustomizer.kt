f690fd7b10a8b6da06ba572e14481b00da3155a1
Jan-Hendrik Peters <jan-hendrik.peters@idealo.de>
2022-12-22 11:58:47 +0100

    log ip address without port in accesslogs

--- src/main/kotlin/name/hennr/cctrayhub/cctray/logging/NettyWebServerAccessLoggingCustomizer.kt
+++ src/main/kotlin/name/hennr/cctrayhub/cctray/logging/NettyWebServerAccessLoggingCustomizer.kt
@@ -4,13 +4,12 @@ import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory
 import org.springframework.boot.web.embedded.netty.NettyServerCustomizer
 import org.springframework.boot.web.server.WebServerFactoryCustomizer
 import org.springframework.context.annotation.Profile
-
 import org.springframework.stereotype.Component
 import reactor.netty.http.server.HttpServer
 import reactor.netty.http.server.logging.AccessLog
 import reactor.netty.http.server.logging.AccessLogArgProvider
+import java.net.InetSocketAddress
 import java.time.format.DateTimeFormatter
-
 
 @Component
 @Profile("json-log-format")
@@ -18,8 +17,9 @@ class NettyWebServerAccessLoggingCustomizer: WebServerFactoryCustomizer<NettyRea
     override fun customize(factory: NettyReactiveWebServerFactory) {
         factory.addServerCustomizers(NettyServerCustomizer { httpServer: HttpServer ->
             httpServer.accessLog(true) { logArguments: AccessLogArgProvider ->
+                val remoteHost = (logArguments.remoteAddress() as? InetSocketAddress)?.address ?: InetSocketAddress("unknown", 6666)
                 AccessLog.create("remoteHost={}, method={}, size={}, code={}, uri={}, duration={}, timestamp={}, host={}, referer={}, user-agent={}, forwarded={}",
-                    logArguments.remoteAddress(),
+                    remoteHost,
                     logArguments.method(),
                     logArguments.contentLength(),
                     logArguments.status(),
