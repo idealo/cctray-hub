524c99ada12b92c1afbf5b19915b8b232a993741
Philipp Thomas <philipp.thomas@idealo.de>
2022-12-23 14:59:32 +0100

    Revert "spring-boot 3.0.1"
    This reverts commit 9b67e18e2715a684ac88e66018a2b8454b663a5c.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.1"
+	id("org.springframework.boot") version "3.0.0"
 	id("io.spring.dependency-management") version "1.1.0"
-	id("org.jetbrains.kotlin.jvm") version "1.7.22"
-	id("org.jetbrains.kotlin.plugin.spring") version "1.7.22"
+	id("org.jetbrains.kotlin.jvm") version "1.7.21"
+	id("org.jetbrains.kotlin.plugin.spring") version "1.7.21"
 	id("com.adarshr.test-logger") version "3.2.0"
 }
 
