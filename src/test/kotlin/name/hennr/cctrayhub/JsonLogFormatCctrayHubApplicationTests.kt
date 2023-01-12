487640dbda5e2f635c53a991ce0849f2d5c4867c
Philipp Thomas <philipp.thomas@idealo.de>
2023-01-12 10:43:33 +0100

    add test for json-log-format profile

---
+++ src/test/kotlin/name/hennr/cctrayhub/JsonLogFormatCctrayHubApplicationTests.kt
@@ -0,0 +1,15 @@
+package name.hennr.cctrayhub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@SpringBootTest
+@ActiveProfiles("json-log-format")
+class JsonLogFormatCctrayHubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
